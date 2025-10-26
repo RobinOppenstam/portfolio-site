f76004e81ec56f37aa4e22439cd3ff84cda47e22
RobinOppenstam <oppenstam.robin@gmail.com>
2025-10-26 15:39:43 +0700

    updates

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,25 @@ const inter = Inter({ subsets: ['latin'] });
 export const metadata: Metadata = {
   title: 'Robin Oppenstam - Portfolio',
   description: 'Full Stack Developer & Web3 fanatic',
+  openGraph: {
+    title: 'Robin Oppenstam - Portfolio',
+    description: 'Full Stack Developer & Web3 fanatic',
+    images: [
+      {
+        url: '/ProfilePicture.png',
+        width: 1200,
+        height: 630,
+        alt: 'Robin Oppenstam Profile Picture',
+      }
+    ],
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Robin Oppenstam - Portfolio',
+    description: 'Full Stack Developer & Web3 fanatic',
+    images: ['/ProfilePicture.png'],
+  },
 };
 
 export default function RootLayout({
